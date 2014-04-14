d350a3b261d542b7368aed0dd983dadf71dce6e6
Matt Knicos <mknicos@gmail.com>
2014-04-13 22:31:40 -0500

    8 tests pass

--- lib/linked_list_item.rb
+++ lib/linked_list_item.rb
@@ -1,5 +1,25 @@
 class LinkedListItem
-  def initialize(item)
+  attr_accessor :payload
+  attr_reader :next_list_item
 
+  # called by LinkedListItem.new
+  def initialize(payload)
+    @payload = payload
+    @next_list_item = nil
   end
+  
+  def next_list_item=(data)
+    if data == self
+      raise ArgumentError
+    else
+      @next_list_item = data
+    end
+  end
+
+  def last?
+    @next_list_item == nil
+    #this returns true if next_list_item has not been set, aka, its last
+  end
+
+
 end
